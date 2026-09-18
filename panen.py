ab8a6c04640c67a6494cd7be1c732bccb6f06a2a
Dzikri Farhanul Hakim <emailmu@gmail.com>
2026-09-18 16:12:07 +0000

    Konflik dari Dzikri

--- panen.py
+++ panen.py
@@ -1,18 +1,5 @@
-print("=== Program Penghitung Hasil Panen ===")
+print("=== PROGRAM UTAMA: SISTEM PANEN RAYA ===") 
 berat = float(input("Masukkan berat panen (kg): "))
 harga_per_kg = float(input("Masukkan harga per kg: Rp"))
-
 total = berat * harga_per_kg
-diskon = 0
-if total > 1000000:
-    diskon = total * 0.10
-total_bersih = total - diskon
-
-print(f"Total pendapatan kotor: Rp{total}")
-print(f"Potongan diskon: Rp{diskon}")
-print(f"Total pendapatan bersih: Rp{total_bersih}")
-
-# Fitur buatan Anggota B
-print("\n====================================")
-print("  LAPORAN CETAK SISTEM PANEN DIGITAL")
-print("====================================")
+print(f"Total pendapatan: Rp{total}")
