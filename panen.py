e09f3074cf5e29a0e9529ec2eb6069a83f308d34
Dzikri Farhanul Hakim <emailmu@gmail.com>
2026-09-18 15:11:13 +0000

    Anggota A: Menambahkan fitur input target panen

--- panen.py
+++ panen.py
@@ -1,16 +1,16 @@
 print("=== Program Penghitung Hasil Panen ===")
+target = float(input("Masukkan target panen bulan ini (kg): ")) # Fitur buatan Dzikri
 berat = float(input("Masukkan berat panen (kg): "))
 harga_per_kg = float(input("Masukkan harga per kg: Rp"))
 
 total = berat * harga_per_kg
-
-# Fitur Diskon: Diskon 10% jika total pendapatan kotor di atas Rp 1.000.000
 diskon = 0
 if total > 1000000:
     diskon = total * 0.10
-    
 total_bersih = total - diskon
 
+print(f"\n--- HASIL ---")
+print(f"Target terpenuhi: {'Ya' if berat >= target else 'Belum'}") # Fitur buatan Dzikri
 print(f"Total pendapatan kotor: Rp{total}")
 print(f"Potongan diskon: Rp{diskon}")
 print(f"Total pendapatan bersih: Rp{total_bersih}")
