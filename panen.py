6562eac11ff49183f97025a6085f90243015eb9e
SITA ASYAHIR <sitaasyahir@gmail.com>
2026-09-18 15:27:07 +0000

    Anggota B: Menambahkan fitur laporan cetak di akhir program

--- panen.py
+++ panen.py
@@ -1,5 +1,4 @@
 print("=== Program Penghitung Hasil Panen ===")
-target = float(input("Masukkan target panen bulan ini (kg): ")) # Fitur buatan Dzikri
 berat = float(input("Masukkan berat panen (kg): "))
 harga_per_kg = float(input("Masukkan harga per kg: Rp"))
 
@@ -9,8 +8,11 @@ if total > 1000000:
     diskon = total * 0.10
 total_bersih = total - diskon
 
-print(f"\n--- HASIL ---")
-print(f"Target terpenuhi: {'Ya' if berat >= target else 'Belum'}") # Fitur buatan Dzikri
 print(f"Total pendapatan kotor: Rp{total}")
 print(f"Potongan diskon: Rp{diskon}")
 print(f"Total pendapatan bersih: Rp{total_bersih}")
+
+# Fitur buatan Anggota B
+print("\n====================================")
+print("  LAPORAN CETAK SISTEM PANEN DIGITAL")
+print("====================================")
