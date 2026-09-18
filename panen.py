39dc1c4e872857932269e76094a8983c1c2015f9
Dzikri Farhanul Hakim <emailmu@gmail.com>
2026-09-18 14:38:15 +0000

    Menambahkan fitur perhitungan diskon 10 persen untuk panen besar

--- panen.py
+++ panen.py
@@ -3,4 +3,14 @@ berat = float(input("Masukkan berat panen (kg): "))
 harga_per_kg = float(input("Masukkan harga per kg: Rp"))
 
 total = berat * harga_per_kg
-print(f"Total pendapatan: Rp{total}")
+
+# Fitur Diskon: Diskon 10% jika total pendapatan kotor di atas Rp 1.000.000
+diskon = 0
+if total > 1000000:
+    diskon = total * 0.10
+    
+total_bersih = total - diskon
+
+print(f"Total pendapatan kotor: Rp{total}")
+print(f"Potongan diskon: Rp{diskon}")
+print(f"Total pendapatan bersih: Rp{total_bersih}")
